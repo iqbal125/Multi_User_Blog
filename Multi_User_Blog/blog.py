8b4b4983cbe9cd7f814f6f478ce1b5c02643beb3
iqbal125 <iqbal125@yahoo.com>
2017-02-02 21:50:45 -0800

    Add files via upload

--- Multi_User_Blog/blog.py
+++ Multi_User_Blog/blog.py
@@ -10,30 +10,31 @@ import hashlib
 import hmac
 from google.appengine.ext import db
 
+
+""" The Main Blog Handler Class and Methods """
+
 #Used to make hashes more secure
 secret = "xsw321"
-
 #Used for connecting templates with jinja2
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 #Creates jinja2 environment for loading templates
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
-
 #Renders templates with jinja2
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
-#Returns a beginning value and a secure hash value, to be used as a cookie in the Handler
+
+#Returns a beginning value and a hmac value, to be used as a cookie in the Handler
 def make_secure_val(val):
     return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
-#Makes sure the beginning values match
+
+#Makes sure hash values match
 def check_secure_val(secure_val):
     val = secure_val.split('|')[0]
     if secure_val == make_secure_val(val):
         return val
-
-
 
 #Basic request handling, slightly modified and copied from Udacity Intro to Backend course
 class Handler(webapp2.RequestHandler):
@@ -63,34 +64,134 @@ class Handler(webapp2.RequestHandler):
     #Sets cookie to None
     def logout(self):
         self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
-    #Reads cookie and makes sure user is logged in for every page
+    #Reads cookie and sets user to that user id
     def initialize(self, *a, **kw):
         webapp2.RequestHandler.initialize(self, *a, **kw)
         uid = self.read_secure_cookie('user_id')
         self.user = uid and User.by_id(int(uid))
 
 
+""" Classes to Handle the Requests """
+
+#Sorts posts on the home page by date created
+class HomePage(Handler):
+    def get(self):
+        posts = db.GqlQuery("select * from Post order by created desc limit 10")
+        self.render('home.html', posts = posts)
 
 class LoginPage(Handler):
-    self.render("login.html")
+    def get(self):
+        self.render("login.html")
+    #Gets username and password
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        #If valid username and password logs in user, otherwise prints error message
+        u = User.login(username, password)
+        if u:
+            self.login(u)
+            self.redirect('/blog')
+        else:
+            msg = 'Invalid login'
+            self.render('login.html', error = msg)
 
-#Logs out user and redirects to homepage
+#Calls logout method in the Handler and redirects to homepage
 class LogoutPage(Handler):
     def get(self):
         self.logout()
         self.redirect('/')
 
-class NewPostPage(Handler):
-    self.render("newpost.html")
-
-class PermaPage(Handler):
-    self.render("permalink.html")
-
-#Sorts posts on the home page by date created
-class HomePage(Handler):
+#Lets user submit a post if they are logged in
+class NewPostPage(BlogHandler):
     def get(self):
-        posts = greetings = Post.all().order('-created')
-        self.render('home.html', posts = posts)
+        if self.user:
+            self.render("newpost.html")
+        else:
+            self.redirect("/login")
+
+    def post(self):
+        if not self.user:
+            self.redirect('/blog')
+
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+        #If there is subject and content, the Post class creates a new post and redirects to a permalink
+        if subject and content:
+            p = Post(parent = blog_key(), subject = subject, content = content)
+            p.put()
+            self.redirect('/blog/%s' % str(p.key().id()))
+        else:
+            error = "Subject and Content, please!"
+            self.render("newpost.html", subject=subject, content=content, error=error)
+
+class PostPage(Handler):
+    def get(self, post_id):
+        #Creates key for specific post
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        #Gets the key for a specific post
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("permalink.html", post = post)
+
+class SignupPage(Handler):
+    def get(self):
+        self.render("sign-up.html")
+
+    def post(self):
+        #Gets username, password, email
+        have_error = False
+        self.username = self.request.get('username')
+        self.password = self.request.get('password')
+        self.verify = self.request.get('verify')
+        self.email = self.request.get('email')
+
+        params = dict(username = self.username,
+                      email = self.email)
+        #Checks if username is valid otherwise returns error
+        if not valid_username(self.username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(self.password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif self.password != self.verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+
+        if not valid_email(self.email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+            self.render('signup-form.html', **params)
+        else:
+            self.done()
+
+    def done(self, *a, **kw):
+        raise NotImplementedError
+
+
+class Register(SignupPage):
+    def done(self):
+        #Makes sure user doesnt already exist, then creates and logs in new user
+        u = User.by_name(self.username)
+        if u:
+            msg = 'That user already exists.'
+            self.render('sign-up.html', error_username = msg)
+        else:
+            u = User.register(self.username, self.password, self.email)
+            u.put()
+
+            self.login(u)
+            self.redirect('/blog')
+
+
+""" Makes password hashes """
 
 #Generates 5 random letters for use in make_pw_hash method
 def make_salt(length = 5):
@@ -103,26 +204,57 @@ def make_pw_hash(name, pw, salt = None):
     h = hashlib.sha256(name + pw + salt).hexdigest()
     return '%s,%s' % (salt, h)
 
-#Compares hash values
+#Makes sure database hash matches user hash
 def valid_pw(name, password, h):
     salt = h.split(',')[0]
     return h == make_pw_hash(name, password, salt)
 
 #Creates a users Key Object
-def users_key(group = 'default'):
-    return db.Key.from_path('users', group)
+def users_key(name):
+    return db.Key.from_path('users', name)
 
 #Creates a blog Key Object
-def blog_key(name):
-    return db.Key.from_path('blogs', name)
+def blog_key(group):
+    return db.Key.from_path('blogs', group)
 
-#Stores information about users
+
+""" The User and Post Models """
+
+#User entity with name, password hash and optional email properties
 class User(db.Model):
     name = db.StringProperty(required = True)
     pw_hash = db.StringProperty(required = True)
     email = db.StringProperty()
 
-#Stores information about posts
+    #Returns a user by their id
+    @classmethod
+    def by_id(cls, uid):
+        return cls.get_by_id(uid, parent = users_key())
+
+    #Returns a user by their name
+    @classmethod
+    def by_name(cls, name):
+        u = cls.all().filter('name =', name).get()
+        return u
+
+    #Returns a new user
+    @classmethod
+    def register(cls, name, pw, email = None):
+        pw_hash = make_pw_hash(name, pw)
+        return cls(parent = users_key(),
+                    name = name,
+                    pw_hash = pw_hash,
+                    email = email)
+
+    #Looks up a user by name, and if the user is valid returns the user
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u
+
+
+#Post entity with subject, content and created date properties
 class Post(db.Model):
     subject = db.StringProperty(required = True)
     content = db.TextProperty(required = True)
@@ -133,23 +265,8 @@ class Post(db.Model):
         self._render_text = self.content.replace('\n', '<br>')
         return render_str("post.html", p = self)
 
-class NewPostPage(BlogHandler):
-    def get(self):
-        if self.user:
-            self.render("newpost.html")
-        else:
-            self.redirect("/login")
 
-    def post(self):
-        if not self.user:
-            self.redirect('/blog')
-
-class PostPage(Handler):
-    self.render("post.html")
-
-class SignupPage(Handler):
-    self.render("sign-up.html")
-
+""" Username and Password Validation """
 
 #Basic valid username and password code, copied from Udacity Intro to Backend course
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
@@ -166,7 +283,13 @@ def valid_email(email):
 
 
 
-#Handles URI Routing
-app = webapp2.WSGIApplication([(
-                               )],
+""" Handles URI Routing """
+
+app = webapp2.WSGIApplication([('/blog/?', HomePage),
+                               ('/blog/([0-9]+)', PostPage),
+                               ('/blog/newpost', NewPostPage),
+                               ('/signup', Register),
+                               ('/login', LoginPage),
+                               ('/logout', LogoutPage)
+                               ],
                               debug=True)
